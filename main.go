378e111a390920e42a49cbec5644795c2a299236
ppmaungdevv <ppmaungdev@gmail.com>
2022-11-06 14:04:46 +0630

    ask user input using scan and pointer

--- main.go
+++ main.go
@@ -3,22 +3,32 @@ package main
 import "fmt"
 
 func main()  {
-	conferenceName := "GO conference" // this can only be used with var not with const and specific data type declaration 
-	const conferenceTickets = 50
-	var remainingTickets uint = 50 // uint => unsigned int
+	conference_name := "GO conference" // this can only be used with var not with const and specific data type declaration 
+	const conference_tickets = 50
+	var remaining_tickets uint = 50 // uint => unsigned int
 
-	// remainingTickets = -1 // this will be an error as the varibale was declared as an unsinged int
+	// remaining_tickets = -1 // this will be an error as the varibale was declared as an unsinged int
 
-	fmt.Printf("conferenceName is a %T, conferenceTickets is an %T, remainingTickets is an %T\n", conferenceName, conferenceTickets, remainingTickets)
+	fmt.Printf("conference_name is a %T, conference_tickets is an %T, remaining_tickets is an %T\n", conference_name, conference_tickets, remaining_tickets)
 
-	fmt.Printf("Welcome %v booking application\n", conferenceName)
-	fmt.Printf("Total Seats: %v\nRemaining Tickets: %v\n", conferenceTickets, remainingTickets )
+	fmt.Printf("Welcome %v booking application\n", conference_name)
+	fmt.Printf("Total Seats: %v\nRemaining Tickets: %v\n", conference_tickets, remaining_tickets )
 	fmt.Println("Get tickets")
 
-	var useName string
-	var userTickets int
-	useName = "Ma"
-	userTickets = 2
-	fmt.Printf("%v booked %v tickets\n", useName, userTickets)
+	fmt.Println(&remaining_tickets) // &remainingTickets is a pointer which is a special variable to point the memory location of a variable
+
+	
+	var full_name string
+	var user_tickets int
+	var email string
+	// when using scan, asking for user input, need to use pointer "&var"
+	fmt.Println("Enter Full Name:")
+	fmt.Scan(&full_name)
+	fmt.Println("Enter Email:")
+	fmt.Scan(&email)
+	fmt.Println("Enter Number of Tickets:")
+	fmt.Scan(&user_tickets)
+
+	fmt.Printf("%v booked %v tickets. Confirmation mail will be sent to %v\n", full_name, user_tickets, email)
 
 }
