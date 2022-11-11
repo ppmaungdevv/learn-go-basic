90c2b15a8fa9b1c8f2fd5b81fa947744a01b4f86
ppmaungdevv <ppmaungdev@gmail.com>
2022-11-11 19:18:54 +0630

    if else part-2

--- main.go
+++ main.go
@@ -53,44 +53,55 @@ func main()  {
 			fmt.Scan(&email)
 			fmt.Println("Enter Number of Tickets:")
 			fmt.Scan(&user_tickets)
+
+			is_valid_name := len(full_name) >= 2
+			is_valid_email := strings.Contains(email, "@")
+			is_valid_ticket := user_tickets > 0 && remaining_tickets < user_tickets
 			
-			// invalid operation will if the variables in the calculation do not have the same types
-			remaining_tickets = remaining_tickets - user_tickets
-
-			if remaining_tickets < user_tickets {
-				fmt.Printf("we only have %v, can't book %v tickets", remaining_tickets, user_tickets)
-				continue 
-				// break will stop the loop
-				// continue will skip all the code below and restart the next iteration
-			}
-
-			// bookings[0] = full_name // use this way to add data to array
-			bookings = append(bookings, full_name) // use this way to add data to slice
-			// bookings[52] = full_name // out of bond error will show
-		
-			fmt.Printf("Array: %v\n", bookings)
-			fmt.Printf("First Element: %v\n", bookings[0])
-			fmt.Printf("Array Type: %T\n", bookings)
-			fmt.Printf("Array length: %v\n", len(bookings))
-			fmt.Printf("%v booked %v tickets. Confirmation mail will be sent to %v\n", full_name, user_tickets, email)
-			fmt.Println("Remaining Tickets: ", remaining_tickets)
-
-			first_names := []string{}
 			
-			// range iterates for different data structure, not only for iterating array and slices
-			// for array & slices, range provides index and value for each element
-			// _ is called blank identifier, used for unused variable and can also be used to call only init function of another package
-			for _, ele := range bookings {
-					var names = strings.Fields(ele) // strings.Fields() splits the string at ' ', strings.Fields('Pyae Phyo M g') => ['Pyae', 'Phyo', 'M', 'g']
-					first_names = append(first_names, names[0])
+			// if remaining_tickets < user_tickets {
+				// 	fmt.Printf("we only have %v, can't book %v tickets", remaining_tickets, user_tickets)
+				// 	continue 
+				// 	// break will stop the loop
+				// 	// continue will skip all the code below and restart the next iteration
+				// }
+				
+			if is_valid_name && is_valid_email && is_valid_ticket {
+				// invalid operation will if the variables in the calculation do not have the same types
+				remaining_tickets = remaining_tickets - user_tickets
+				// bookings[0] = full_name // use this way to add data to array
+				bookings = append(bookings, full_name) // use this way to add data to slice
+				// bookings[52] = full_name // out of bond error will show
+			
+				fmt.Printf("Array: %v\n", bookings)
+				fmt.Printf("First Element: %v\n", bookings[0])
+				fmt.Printf("Array Type: %T\n", bookings)
+				fmt.Printf("Array length: %v\n", len(bookings))
+				fmt.Printf("%v booked %v tickets. Confirmation mail will be sent to %v\n", full_name, user_tickets, email)
+				fmt.Println("Remaining Tickets: ", remaining_tickets)
+	
+				first_names := []string{}
+				
+				// range iterates for different data structure, not only for iterating array and slices
+				// for array & slices, range provides index and value for each element
+				// _ is called blank identifier, used for unused variable and can also be used to call only init function of another package
+				for _, ele := range bookings {
+						var names = strings.Fields(ele) // strings.Fields() splits the string at ' ', strings.Fields('Pyae Phyo M g') => ['Pyae', 'Phyo', 'M', 'g']
+						first_names = append(first_names, names[0])
+				}
+	
+				fmt.Printf("First Names: %v", first_names)
+				// var no_tickets bool = remaining_tickets == 0 // boolean data type
+				if remaining_tickets == 0 {
+					fmt.Println("Booked out!")
+					break
+				}
+				
+			} else {
+				fmt.Println("Invalid Entry! Try again.")
+				continue
 			}
 
-			fmt.Printf("First Names: %v", first_names)
-			// var no_tickets bool = remaining_tickets == 0 // boolean data type
-			if remaining_tickets == 0 {
-				fmt.Println("Booked out!")
-				break
-			}
 	}
 
 }
